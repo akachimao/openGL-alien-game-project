e69a2d45e241f0a530b6518e5cb189fbc1806959
EnacheDavid <simpamimpa@gmail.com>
2025-11-16 23:53:44 +0200

    Update Player.cpp

--- 2D/Player.cpp
+++ 2D/Player.cpp
@@ -2,15 +2,14 @@
 
 Player::Player(glm::vec3 pos, float sz)
     : position(pos),
-      velocity(0.0f, 0.0f, 0.0f),
-      size(sz),
-      speed(0.01f),           
-      jumpForce(0.04f),      
-      gravity(-0.001f),      
-      isGrounded(false),     
-      color(0.5f, 0.0f, 0.5f, 1.0f) 
+    velocity(0.0f, 0.0f, 0.0f),
+    size(sz),
+    speed(0.01f),
+    jumpForce(0.04f),
+    gravity(-0.001f),
+    isGrounded(false),
+    color(0.7f, 0.7f, 0.7f, 1.0f)  // Light gray for astronaut
 {
-    
 }
 
 void Player::handleInput(GLFWwindow* window) {
@@ -21,7 +20,6 @@ void Player::handleInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
         position.x += speed;
     }
-
     // Jump
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS && isGrounded) {
         velocity.y = jumpForce;
@@ -30,19 +28,87 @@ void Player::handleInput(GLFWwindow* window) {
 }
 
 void Player::update() {
-    
     velocity.y += gravity;
     position.y += velocity.y;
 }
 
 void Player::draw(GLuint transformLoc, GLuint colorLoc, glm::mat4 view) {
-    glm::mat4 transform = glm::mat4(1.0f);
-    transform = glm::translate(transform, position);
-    transform = glm::scale(transform, glm::vec3(size, size, 1.0f));
-    transform = view * transform;
+    glm::vec4 lightGray = glm::vec4(0.7f, 0.7f, 0.7f, 1.0f);  // Astronaut body color
+    glm::vec4 blackVisor = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);  // Visor color
 
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-    glUniform4fv(colorLoc, 1, glm::value_ptr(color));
+    // If player died (collision), use the death color for entire astronaut
+    if (color.r > 0.9f && color.g < 0.3f) {  // Red (death)
+        lightGray = color;
+        blackVisor = color;
+    }
+    else if (color.b > 0.9f && color.r < 0.3f) {  // Blue (win)
+        lightGray = color;
+        blackVisor = color;
+    }
+
+    // === LEGS ===
+    // Left leg
+    glm::mat4 leftLeg = glm::mat4(1.0f);
+    leftLeg = glm::translate(leftLeg, position + glm::vec3(-size * 0.15f, -size * 0.6f, 0.0f));
+    leftLeg = glm::scale(leftLeg, glm::vec3(size * 0.25f, size * 0.4f, 1.0f));
+    leftLeg = view * leftLeg;
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(leftLeg));
+    glUniform4fv(colorLoc, 1, glm::value_ptr(lightGray));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // Right leg
+    glm::mat4 rightLeg = glm::mat4(1.0f);
+    rightLeg = glm::translate(rightLeg, position + glm::vec3(size * 0.15f, -size * 0.6f, 0.0f));
+    rightLeg = glm::scale(rightLeg, glm::vec3(size * 0.25f, size * 0.4f, 1.0f));
+    rightLeg = view * rightLeg;
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(rightLeg));
+    glUniform4fv(colorLoc, 1, glm::value_ptr(lightGray));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // === TORSO (main body) ===
+    glm::mat4 torso = glm::mat4(1.0f);
+    torso = glm::translate(torso, position + glm::vec3(0.0f, -size * 0.1f, 0.0f));
+    torso = glm::scale(torso, glm::vec3(size * 0.6f, size * 0.7f, 1.0f));
+    torso = view * torso;
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(torso));
+    glUniform4fv(colorLoc, 1, glm::value_ptr(lightGray));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // === ARMS ===
+    // Left arm
+    glm::mat4 leftArm = glm::mat4(1.0f);
+    leftArm = glm::translate(leftArm, position + glm::vec3(-size * 0.4f, -size * 0.05f, 0.0f));
+    leftArm = glm::scale(leftArm, glm::vec3(size * 0.2f, size * 0.5f, 1.0f));
+    leftArm = view * leftArm;
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(leftArm));
+    glUniform4fv(colorLoc, 1, glm::value_ptr(lightGray));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // Right arm
+    glm::mat4 rightArm = glm::mat4(1.0f);
+    rightArm = glm::translate(rightArm, position + glm::vec3(size * 0.4f, -size * 0.05f, 0.0f));
+    rightArm = glm::scale(rightArm, glm::vec3(size * 0.2f, size * 0.5f, 1.0f));
+    rightArm = view * rightArm;
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(rightArm));
+    glUniform4fv(colorLoc, 1, glm::value_ptr(lightGray));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // === HEAD ===
+    glm::mat4 head = glm::mat4(1.0f);
+    head = glm::translate(head, position + glm::vec3(0.0f, size * 0.4f, 0.0f));
+    head = glm::scale(head, glm::vec3(size * 0.45f, size * 0.45f, 1.0f));
+    head = view * head;
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(head));
+    glUniform4fv(colorLoc, 1, glm::value_ptr(lightGray));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // === VISOR (black square on head) ===
+    glm::mat4 visor = glm::mat4(1.0f);
+    visor = glm::translate(visor, position + glm::vec3(0.0f, size * 0.4f, 0.0f));
+    visor = glm::scale(visor, glm::vec3(size * 0.35f, size * 0.2f, 1.0f));
+    visor = view * visor;
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(visor));
+    glUniform4fv(colorLoc, 1, glm::value_ptr(blackVisor));
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
